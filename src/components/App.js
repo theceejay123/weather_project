c66383876677e000dc2326836221e3f4c32520ba
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-07-01 22:58:00 -0500

    Adds a new index and App components

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="barContainer">
+        <h1>Weather</h1>
+        <p>Search Bar</p>
+      </div>
+      <div className="weatherContainer">
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
