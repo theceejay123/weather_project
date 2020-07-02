45d2ec67d7e7a1e7e73d0c795f8dc01904668939
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-07-02 12:34:41 -0500

    Adds CSS files and SearchBar API connection

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ props }) => {
+
+  const searchData = () => {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=winnipeg&appid=daddc13f21c2e3b7b755ba12eb40b020")
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        }
+      })
+      .then(data => {
+        props.setForecast(data)
+      })
+  };
+
   return (
     <div className="searchBarContainer">
-      <p>searchbar</p>
+      <button onClick={searchData}>Click Me</button>
     </div>
   );
 }
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Weather = () => {
+const Weather = ({ props }) => {
   return (
     <div className="weatherContainer">
-
+      {console.log(props.forecast)}
     </div>
   );
 }
